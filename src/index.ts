4938020321e35e2109f676bb4453bc2a50a4cc46
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-12-06 23:48:41 -0300

    Progeto buscando todos os usuarios da lista

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app } from "./app"
 import { newUser } from "./services/newUser"
+import { search } from "./services/search"
 
 app.post("/users", newUser )
+app.get("/users", search)
---
+++ src/services/search.ts
@@ -0,0 +1,13 @@
+import { connection } from '../connection/connection'
+import { Request, Response } from 'express'
+
+export async function search (req: Request, res: Response) {
+    try{
+        let result = await connection ("labecommerce_users").select()
+
+        res.status(200).send(result)
+
+    }catch(error){
+    res.status(400).send(error)
+}
+}
