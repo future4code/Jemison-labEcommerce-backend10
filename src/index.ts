80f23cb1b65a87ec71fdb2e69505bd69212ce0e2
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-12-07 19:16:18 -0300

    Adicionando produtos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { app } from "./app"
 import { newUser } from "./services/newUser"
+import { product } from "./services/product"
 import { search } from "./services/search"
 
-app.post("/users", newUser )
+app.post("/users", newUser)
 app.get("/users", search)
+app.post("/products", product)
